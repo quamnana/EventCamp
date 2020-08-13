67ecd7995c9c1b3ed37dc2ca2408127a60607f5f
kobby08 <linusquam@gmail.com>
2020-08-13 09:12:01 +0000

    mailing with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => ENV['GMAIL_USERNAME'],
- :password             => ENV['GMAIL_PASSWORD'],
+ :user_name            => "eventcamp08@gmail.com",
+ :password             => "gotze008",
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
